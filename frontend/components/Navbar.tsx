838e5ef5cefe9272034a5deef12686f4eee4e263
sz-attila <szalai.attila2002@gmail.com>
2024-05-22 14:53:40 +0200

    Adding responsivity to the Navbar with the required style

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,28 +1,52 @@
 "use client";
+import { useState } from "react";
 import Link from "next/link";
-import React from "react";
+import { usePathname } from "next/navigation";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <div>
-        <img src="/logo2.svg" alt="Taskify Logo" />
-        <span>TASKLIFY</span>
-      </div>
-      <ul>
-        <li>
-          <Link href="">TEENDŐIM</Link>
-        </li>
-        <li>
-          <Link href="">DASHBOARD</Link>
-        </li>
-        <li>
+    <nav className={isMenuOpen ? "active" : ""}>
+      <div className="nav-container">
+        <div className="nav-logo">
+          <img src="/logo2.svg" alt="Taskify Logo" />
+          <span>TASKLIFY</span>
+          <div className="nav-menu-icon" onClick={toggleMenu}>
+            <img
+              src={isMenuOpen ? "/cross.svg" : "/menu.svg"}
+              alt="Menu Icon"
+            />
+          </div>
+        </div>
+        <ul className={`nav-links ${isMenuOpen ? "active" : ""}`}>
+          <li>
+            <Link
+              href="/tasks"
+              className={pathname === "/tasks" ? "active" : ""}
+            >
+              TEENDŐIM
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dashboard"
+              className={pathname === "/dashboard" ? "active" : ""}
+            >
+              DASHBOARD
+            </Link>
+          </li>
           <Link href="/">
-            <Button>BEJELENTKEZÉS</Button>
+            <Button className="button">BEJELENTKEZÉS</Button>
           </Link>
-        </li>
-      </ul>
+        </ul>
+      </div>
     </nav>
   );
 };
